7f9ce83d0ee98f0ced92ffe927d729fda2ec0df5
omar-hossam <omarhossam50@proton.me>
2026-04-12 19:46:09 +0200

    improve: 800/4A write both solutions to compare between logic and math

--- 800/4A.py
+++ 800/4A.py
@@ -1,5 +1,19 @@
+# Version 1: Brute force (my first solution)
+def can_divide_brute(weight: int) -> str:
+    if not weight % 2: # if weight is even
+        # Loop through `range(weight)` to find 2 even numbers that sum to weight
+        for i in range(weight): 
+            for j in range(weight):
+                if not i % 2 and not j % 2 and i + j == weight:
+                    return "YES"
+        return "NO" 
+    return "NO" # if weight is odd
+
+
+# Version 2: Mathematical 0(1) (after learning)
 def can_divide(weight: int) -> str:
-    return "YES" if weight > 2 and weight % 2 == 0 else "NO"
+    return "YES" if weight > 2 and weight % 2 == 0 else "NO" 
+
 
 def main() -> None:
     try:
